d618901a0b7555d5ec7539822862903108629bb2
Jake Garcia <bballwiz4@gmail.com>
2020-03-24 04:18:51 +0000

    Commit fo ruby gem to work

--- lib/npr_cli_news_reader.rb
+++ lib/npr_cli_news_reader.rb
@@ -1,10 +1,11 @@
-require_relative "./npr_cli_news_reader/version"
-require_relative "./npr_cli_news_reader/cli"
-require_relative "./npr_cli_news_reader/scraper"
-require_relative "./npr_cli_news_reader/article"
+require_relative './npr_cli_news_reader/article'
+require_relative './npr_cli_news_reader/cli'
+require_relative './npr_cli_news_reader/run'
+require_relative './npr_cli_news_reader/scraper'
 require 'open-uri'
 require 'nokogiri'
 require 'rainbow'
+
 
 module NprCliNewsReader
   class Error < StandardError; end
