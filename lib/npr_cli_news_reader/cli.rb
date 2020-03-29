ef147c745017e3fc06cacf1183949acbe341023c
Jake Garcia <bballwiz4@gmail.com>
2020-03-29 04:03:36 +0000

    the scraper now checks if it already has articles with the selected category.

--- lib/npr_cli_news_reader/cli.rb
+++ lib/npr_cli_news_reader/cli.rb
@@ -1,19 +1,15 @@
 class NprCliNewsReader::CLI 
   
-  attr_accessor :done, :selected_category
+  attr_accessor :selected_category
   
   def initialize
     @categories = ["National", "World", "Politics", "Business", "Health", "Science", "Technology", "Race & Culture"]
-    @done = false
   end
   
   def call
-    
-    # let the user select an article
-    # display the full article.
     greet_user
     present_categories
-    display_category_articles
+    handle_category_input
     handle_article_selection
   end  
   
@@ -49,26 +45,24 @@ class NprCliNewsReader::CLI
     formatted_teaser
   end
   
-  def display_category_articles
-    
+  def handle_category_input
     puts Rainbow("Which Category would you like to see articles for?").bright
     puts "Use the numbers associated with the categories."
     category_input = gets.strip
-    
     while !valid_input?(category_input, @categories)
-    
       puts "Sorry, the input received is not valid to select a category"
       puts "Please enter a number 1-#{@categories.size}:"
       category_input = gets.strip  
-      
     end
-    
     @selected_category = @categories[category_input.to_i - 1].downcase
-    
     puts Rainbow("#{@selected_category.capitalize} Category").bright
     puts "--------------------"
     NprCliNewsReader::Scraper.scrape_articles_for_category(@selected_category)
-    NprCliNewsReader::Article.all.each_with_index do |article, i|
+    display_category_articles
+  end
+  
+  def display_category_articles
+    NprCliNewsReader::Article.sort_articles_by_category(@selected_category).each_with_index do |article, i|
       formatted_teaser = format_teaser(article.teaser)
       if i % 2 == 0
         puts Rainbow("#{i + 1}").bright + ")." + Rainbow(" #{article.title}").bright
@@ -89,37 +83,26 @@ class NprCliNewsReader::CLI
   end
   
   def handle_article_selection
-    # load up a sorted array of articles with the selected category matching
-    # ask the user for input
-    # send the selected article to the display_full_article insatnce method
     sorted_articles = NprCliNewsReader::Article.sort_articles_by_category(@selected_category)
     puts Rainbow("Which article would you like to see the full article for?").bright
     puts "Use the numbers associated with the articles."
     article_input = gets.strip
-    
     while !valid_input?(article_input, sorted_articles)
       puts "Sorry, the input received is not valid to select an article"
       puts "Please enter a number 1-#{sorted_articles.size}:"
       article_input = gets.strip
     end
-    
     article = sorted_articles[article_input.to_i - 1]
-    
     NprCliNewsReader::Scraper.scrape_full_article(article)
-    
     display_full_article(article)
-    
   end
   
   def display_full_article(article)
-    
     puts Rainbow("#{article.title}").bright
     puts "--------------------"
     article.full_article.each do |paragraph_node| 
       puts "\t#{paragraph_node.text}"
       puts "--------------------"
     end
-    
   end
-  
 end
--- lib/npr_cli_news_reader/scraper.rb
+++ lib/npr_cli_news_reader/scraper.rb
@@ -3,18 +3,23 @@ class NprCliNewsReader::Scraper
   @@base_url = "https://www.npr.org"
   
   def self.scrape_articles_for_category(selected_category)
-    html = open("#{@@base_url}/sections/#{selected_category.downcase}")
-    doc = Nokogiri::HTML(html)
-    articles = doc.css('article.item')
-    articles.each do |article|
-      article_attributes = {
-        category: selected_category.downcase,
-        title: article.css('h2.title a').text.strip,
-        teaser: article.css('p.teaser a').text.strip,
-        article_url: article.css('h2.title a').attr('href').to_s.strip
-      }
-      NprCliNewsReader::Article.new(article_attributes)
+    if NprCliNewsReader::Article.all.detect {|article| article.category == selected_category}
+      NprCliNewsReader::Article.scrape_articles_by_category
+    else
+      html = open("#{@@base_url}/sections/#{selected_category}")
+      doc = Nokogiri::HTML(html)
+      articles = doc.css('article.item')
+      articles.each do |article|
+        article_attributes = {
+          category: selected_category.downcase,
+          title: article.css('h2.title a').text.strip,
+          teaser: article.css('p.teaser a').text.strip,
+          article_url: article.css('h2.title a').attr('href').to_s.strip
+        }
+        NprCliNewsReader::Article.new(article_attributes)
+      end
     end
+    
   end
   
   def self.scrape_full_article(article)
